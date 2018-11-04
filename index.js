1416d70e28208029a922c4fb702bf69501f70fd5
Oli Lalonde <olalonde@gmail.com>
2018-11-04 22:24:38 +0800

    also parse failure messages that start with Error

--- index.js
+++ index.js
@@ -2,9 +2,35 @@ class JestVimReporter {
   onTestResult(test, { testResults }) {
     console.log();
     testResults.forEach(test => {
-      if ('failed' === test.status) {
-        console.log(test.failureMessages[0].replace(/\n/g, ' ').replace(/.*?(?=Expected)(.*?)at .*?\((.*?)\).*/, '$1\n($2)').replace(/ +/g, ' '));
-      }
+      if (test.status !== 'failed') return;
+      // note: I couldn't find out how to make a test generate more than one
+      // failure message
+      test.failureMessages.forEach(failureMessage => {
+        const msg = failureMessage
+          .replace(/\n/g, ' ')
+          .replace(/.*?(?=Expected)(.*?)at .*?\((.*?)\).*/, '$1\n($2)')
+          .replace(/.*?(?=Error)(.*?)at .*?\((.*?)\).*/, '$1\n($2)')
+          .replace(/ +/g, ' ')
+          .trim();
+
+        const lines = msg.split('\n');
+        if (lines.length !== 2) {
+          console.error(
+            'vim-test-jest-clean-qf-reporter: could not parse error.'
+          );
+          console.error(
+            'Report the issue here: https://github.com/ajcrites/vim-test-jest-clean-qf-reporter/issues'
+          );
+          console.error(test.failureMessages[0]);
+        } else {
+          const [line1, line2] = lines;
+          const msg = line1.trim();
+          // const location = line2.replace(/^[ ]*\(/, '').replace(/\)[ ]*/, '');
+          const location = line2;
+          console.log(msg);
+          console.log(location);
+        }
+      });
     });
   }
 }
